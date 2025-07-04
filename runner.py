27026251fa71b9e9a1f4693a044f1d64e1ee495b
BlankAdventure <>
2025-07-04 16:31:44 -0400

    minor clean-up in runner.py

--- runner.py
+++ runner.py
@@ -13,17 +13,18 @@ parser.add_argument('-db', default=None, help='URL to db. Defaults to memory is 
 parser.add_argument('-r','--route', help='Optional non-default route to host the site')
 args = parser.parse_args()
 
-
+# The idea below is that if a non-default route is provided, then the 
+# the default route (/) will just redirect to github.
 if args.route:
     print(f'setting new route: {args.route}')
     app.site_base = args.route
     @ui.page('/')
     def redir():
-        ui.navigate.to('https://github.com/blankAdventure/')
-         
+        ui.navigate.to('https://github.com/blankAdventure/')         
 else:
     print('Using default route')
-    
+
+# if a db path is provided, use it. Otherwise run app with in-memory db.    
 if args.db:
     print(f'Running with persisted db: {args.db}')
     app.run_app(args.db)
@@ -31,11 +32,7 @@ else:
     print('Running with memory db')
     app.run_app_memory()
     
-
-ui.run(storage_secret='testtest', reload=False)
-
+ui.run(storage_secret='testtest', reload=False, port=5000)
 
 
-#print(args.production)
 
-#print(args.route)
